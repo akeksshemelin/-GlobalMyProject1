85152396e406479322846ba4d2d1eb1346e24e3c
sasha shem <ashemelin00@mail.ru>
2019-12-11 18:31:26 +0300

    div

--- funcs.h
+++ funcs.h
@@ -3,6 +3,7 @@
 #define __FUNCS_H__
 double MySum(double, double);
 double MySub(double, double);
-double MyMul(double, double);
+double MyMul(double, double);
+double MyDiv(double, double);
 #endif // __FUNCS_H__
 #pragma once
